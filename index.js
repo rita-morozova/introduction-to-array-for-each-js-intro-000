c9470a048450b9b2123088b739ec0028189e3a3c
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-27 17:39:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-
 function doToElementsInArray(array, callback){
   array.forEach(callback);
 }
 
 
-function changeCompletely(){
-  
+function changeCompletely(element, index, array){
+  array[index] = Math.floor(Math.random() * 100+2).toString() + `${array[index]}s!!!`;
 }
